b55bf0f99b435243791cd65776fc33b90aa2f191
Javier A. Pérez Borges <javiborges95@gmail.com>
2021-11-17 09:48:27 -0500

    Starting a new Web App with React. So far it just has a button that adds a predefined name to the ordered list displayed. This is just a test and will be deleted afterwards

---
+++ src/GifExpertApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    
+    const handleAdd = () => {
+        // categories.push('HunterXHunter'); //no sirve para nuestro propósito de agregar un nuevo elemento al array que sea renderizable, lo que hace es agregar un nuevo elemento al objeto array, pero sin actualizarlo en el Hook useState. 
+        // setCategories('HunterXHunter'); //tampoco sirve de esta forma pues lo que hace es sobreescribir "categories" con un elemento de tipo string.
+        setCategories([...categories, 'HunterXHunter']); //opción 1 (utilizando el operador spread)
+        // setCategories( () => [...categories, 'HunterXHunter']); //opción 2
+    };
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={category}> {category} </li>; //'key' debe ser único, porque será la forma de referirnos a un elemento específico. NOTA: no se debe usar el índice que proporciona el segundo argumento de la función map (que en este caso se omite, porque no se necesita).
+                    })
+                }
+            </ol>
+        </>
+    )
+}
