be93893b6d8969dde3756b2fbf3ba4336802a0e4
Javier A. Pérez Borges <javiborges95@gmail.com>
2021-11-20 13:38:03 -0500

    Finished!

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,25 @@
 
 import React from 'react';
 // import {useState, useEffect} from 'react';
-// import { GifGridItem } from './GifGridItem';
-// import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
 
-    // const [images, setImages] = useState([]);
- 
-    // useEffect(() => { //hook
-    //     getGifs(category).then( imgs => setImages(imgs) ); //helper that returns a promise
-    // }, [category]); //saying that if category changes (that will not), the instrucción inside useEffect will be re-runned.
-
-    const {loading} = useFetchGifs();
-
+    const {data:images, loading} = useFetchGifs( category );
 
     return (
         <>
-            <h3> {category} </h3>
-            { loading? 'Cargando': 'Data cargada' }
-            {/* <div className="card-grid">
+            <h3 className="animate__animated animate__fadeIn"> {category} </h3>
+            { loading && <p className="animate__animated animate__flash">Loading</p>}
+            <div className="card-grid">
                 {
                     images.map( img => (
                         // <GifGridItem key={img.id} img={img} />  //opción 1
                         <GifGridItem key={img.id} {...img} />   //opción 2 
                         ))  
                     }
-            </div> */}
+            </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {getGifs} from '../helpers/getGifs'
 
-export const useFetchGifs = () => { //this is a custom hook
+export const useFetchGifs = (category) => { //this is a custom hook
 
     const [state, setState] = useState({
         data: [],
         loading: true
     });
 
-    setTimeout(() => {
-        setState({
-            data: [1,2,3,4,5],
-            loading: false
-        });
-    }, 3000);
+    useEffect(() => { 
+        getGifs(category).then( imgs => setState({
+                data: imgs,
+                loading: false    
+            }) ); //helper that returns a promise
+    }, [category]); //saying that if category changes (that will not), the instrucción inside useEffect will be re-runned.
+    // en el hook de arriba, la función del primer argumento no puede ser async, porque espera algo síncrono    
 
     return state;
 }
