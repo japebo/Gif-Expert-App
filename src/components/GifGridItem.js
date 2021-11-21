96d2ede20dc71289241d5d19d323c16c82edada2
Javier A. Pérez Borges <javiborges95@gmail.com>
2021-11-21 11:39:17 -0500

    In GifGridItems: changed the fadeIn effect from animate.css for a custom shake effect that looks nice.

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({title, url}) => {
     //  console.log(title, url);
 
     return (
-        <div className="card animate__animated animate__fadeIn">
+        <div className="card">
             <img src={url} alt={title}/>
             <p>{title}</p>
         </div>
