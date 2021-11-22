6bd44d34fe1a2cd24e08df738168a9e79e0107fb
Javier A. Pérez Borges <javiborges95@gmail.com>
2021-11-21 23:49:04 -0500

    Starting to test the app.

---
+++ src/tests/GifGridItem.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from "../components/GifGridItem";
+
+describe('Pruebas hechas sobre el componente <GifGridItem />', () => {
+    
+    const title = "Un título";
+    const url = "http://localhost/algo.jpg"
+    let wrapper = shallow(<GifGridItem title={title} url={url}/>);
+    
+    test('Debe retornar el componente correctamente', () => {
+    
+        expect(wrapper).toMatchSnapshot();
+
+    })
+
+    test('Debe tener un párrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe(title);
+    })
+    
+    test('Las propiedades src y alt de la etiqueta img deben tener tener los valores de las constantes title y url definidas en las pruebas', () => {
+        const img = wrapper.find('img');
+        // console.log(img.html()); //imprime en una línea la etiqueta img del html
+        // console.log(img.props().src); //o
+        // console.log(img.prop('src'));
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+    
+})
---
+++ src/tests/getGifs.test.js
@@ -0,0 +1,16 @@
+import { getGifs } from "../helpers/getGifs"
+
+describe('Pruebas con getGifs', () => {
+    test('Debe retornar 10 elementos', async () => {
+        const gifs = await getGifs('One Punch')
+
+        expect(gifs.length).toBe(10);
+    })
+
+    test('Debe retornar 0 elementos', async () => {
+        const gifs = await getGifs('')
+        // console.log(gifs)
+        expect(gifs.length).toBe(0);
+    })
+    
+})
